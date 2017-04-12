b3ab1329d1b9000e337c218462a3d05348373784
ethrvander131 <ethrvander@gmail.com>
2017-04-12 07:54:34 -0400

    Added Infoboard fragment

---
+++ app/src/main/java/ca/tdchristian/tdsplash/fragments/InfoBoardFragment.java
@@ -0,0 +1,35 @@
+package ca.tdchristian.tdsplash.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.tdchristian.tdsplash.R;
+
+
+public class InfoBoardFragment extends Fragment {
+
+    public InfoBoardFragment() {
+        // Required empty public constructor
+    }
+
+    public static InfoBoardFragment newInstance() {
+        return new InfoBoardFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info_board, container, false);
+    }
+
+}
