d34d42259f2190aa20c5e1de10eed89b9cee24aa
Joshua <joshua.guinness@gmail.com>
2017-04-18 14:39:25 -0400

    Added 3 links.

--- app/src/main/java/ca/tdchristian/tdsplash/fragments/MainFragment.java
+++ app/src/main/java/ca/tdchristian/tdsplash/fragments/MainFragment.java
@@ -2,10 +2,13 @@ package ca.tdchristian.tdsplash.fragments;
 
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import ca.tdchristian.tdsplash.R;
 
@@ -15,6 +18,11 @@ import ca.tdchristian.tdsplash.R;
  * create an instance of this fragment.
  */
 public class MainFragment extends Fragment {
+
+    // Variable Declaration and Initialization
+    public Button edsbyButton;
+    public Button splashButton;
+    public Button busButton;
 
     public MainFragment() {
         // Required empty public constructor
@@ -33,7 +41,66 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Button Initialization
+        edsbyButton = (Button)v.findViewById(R.id.edsbyButton);
+        splashButton = (Button)v.findViewById(R.id.splashButton);
+        busButton = (Button)v.findViewById(R.id.busButton);
+
+        // Action Preformed for Edsby Button
+        edsbyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Sets URL and then runs method
+                String url = "https://tdchristian.edsby.com/";
+                openInternet(url);
+
+            }
+        });
+
+        // Action Preformed for Splash Button
+        splashButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Sets URL and then runs method
+                String url = "http://splash.tdchristian.ca/";
+                openInternet(url);
+
+            }
+        });
+
+        // Action Preformed for Webmail Button
+        busButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Sets URL and then runs method
+                String url = "https://tdch.mybusplanner.ca/StudentLogin.aspx";
+                openInternet(url);
+
+            }
+        });
+        return v;
+    }
+
+    // Method which opens up the site in default browser
+    public void openInternet(String url){
+
+        // Changes string to URI
+        Uri.parse(url);
+
+        // Creates a new Intent
+        Intent openBrowser = new Intent(Intent.ACTION_VIEW);
+
+        // Sets information, which is the site URL
+        openBrowser.setData(Uri.parse(url));
+
+        // Opens up the site
+        startActivity(openBrowser);
+
     }
 
 }
