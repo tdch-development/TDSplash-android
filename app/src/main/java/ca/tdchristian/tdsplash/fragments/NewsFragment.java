2c20a068b00d4835500ecdb3b8dc4f2eca061bd3
Tim <timothy.elgersma@gmail.com>
2017-04-25 21:40:17 -0400

    Re-push of a previous update

--- app/src/main/java/ca/tdchristian/tdsplash/fragments/NewsFragment.java
+++ app/src/main/java/ca/tdchristian/tdsplash/fragments/NewsFragment.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
